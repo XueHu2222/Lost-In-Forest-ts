afe5c5f6890e2b06f2690fc815b174a32b731c26
Naimi de Jong <jong0394@hz.nl>
2025-01-08 11:16:38 +0100

    Finished start page, with all buttons and values

--- src/LostInTheForest.ts
+++ src/LostInTheForest.ts
@@ -24,7 +24,7 @@ export default class LostInTheForest extends Game {
     this.canvas.width = window.innerWidth;
     this.mouseListener = new MouseListener(canvas);
     this.player = new Player;
-    this.currentStage = new StartScreen(this.setIsDutch, this.player, canvas);
+    this.currentStage = new StartScreen(this.setIsDutch.bind(this), this.player, canvas);
     this.isDutch = false;
   }
 
--- src/Screens/StartScreen.ts
+++ src/Screens/StartScreen.ts
@@ -5,7 +5,7 @@ import MouseListener from '../MouseListener.js';
 import Player from '../Player.js';
 import Stage from '../Stage.js';
 
-export default class StartScreen extends Stage{
+export default class StartScreen extends Stage {
   private setIsDutch: (value: boolean) => void;
 
   private genderButtons: Button[];
@@ -16,34 +16,65 @@ export default class StartScreen extends Stage{
 
   private started: boolean = false;
 
+  private selectedImage: HTMLImageElement;
+
+  private selectedGender: Button;
+
+  private selectedFlag: Button;
+
   private canvas: HTMLCanvasElement;
 
   public constructor(setIsDutch: (value: boolean) => void,
-    player: Player, canvas: HTMLCanvasElement){
+    player: Player, canvas: HTMLCanvasElement) {
     super(player, false);
-
+    
+    //to give everything a standard value
     this.canvas = canvas;
 
     this.setIsDutch = setIsDutch;
 
-    // TODO: Set the correct button positions
-    const boy: Button = new Button(null, 0, 0, null, 100, 100);
-    const girl: Button = new Button(null, 0, 0, null, 100, 100);
-    const nonBinary: Button = new Button(null, 0, 0, null, 100, 100);
+    this.selectedImage = CanvasRenderer.loadNewImage('./assets/selected.png');
+
+    this.selectedGender= new Button(null, 0, 0, null, 100, 100);
+
+    this.selectedFlag= new Button(null, 0, 0, null, 100, 100);
+
+    //to add images for the button
+    const girlImageButton: HTMLImageElement = CanvasRenderer.loadNewImage('./assets/girlButton.png');
+    const boyImageButton: HTMLImageElement = CanvasRenderer.loadNewImage('./assets/boyButton.png');
+    const nonBinaryImageButton: HTMLImageElement = CanvasRenderer.loadNewImage('./assets/nonbinaireButton.png');
+    //creating gender buttons
+    const boy: Button = new Button(null, canvas.width * 0.35, canvas.height * 0.3,
+      boyImageButton, canvas.width * 0.075, canvas.height * 0.15);
+    const girl: Button = new Button(null, canvas.width * 0.4625, canvas.height * 0.3,
+      girlImageButton, canvas.width * 0.075, canvas.height * 0.15);
+    const nonBinary: Button = new Button(null, canvas.width * 0.575, canvas.height * 0.3,
+      nonBinaryImageButton, canvas.width * 0.075, canvas.height * 0.15);
     this.genderButtons = [boy, girl, nonBinary];
 
-    const dutch: Button = new Button(null, 0, 0, null, 100, 100);
-    const english: Button = new Button(null, 0, 0, null, 100, 100);
+    //images for flag buttons
+    const dutchImageButton: HTMLImageElement = CanvasRenderer.loadNewImage('./assets/nlFlagButton.png');
+    const englishImageButton: HTMLImageElement = CanvasRenderer.loadNewImage('./assets/enFlagButton.png');
+    //creating flag buttons
+    const dutch: Button = new Button(null, canvas.width * 0.35, canvas.height * 0.55,
+      dutchImageButton, canvas.width * 0.15, canvas.height * 0.15);
+    const english: Button = new Button(null, canvas.width * 0.5, canvas.height * 0.55,
+      englishImageButton, canvas.width * 0.15, canvas.height * 0.15);
     this.languageButtons = [dutch, english];
 
+    //start button
     const startImageButton: HTMLImageElement = CanvasRenderer.loadNewImage('./assets/start-buttonstart.png');
-    this.startButton = new Button(null, canvas.width *0.35, canvas.height * 0.8, startImageButton,
+    this.startButton = new Button(null, canvas.width * 0.35, canvas.height * 0.8, startImageButton,
       canvas.width * 0.3, canvas.height * 0.2);
     this.backgroundImage = CanvasRenderer.loadNewImage('./assets/start.png');
   }
 
+  /**
+   * checks if its started to get next stage
+   * @returns the stage if it started or nothing if its not started yet
+   */
   public override getNextStage(): Stage | null {
-    if (this.started){
+    if (this.started) {
       return new BeginCutScene(this.player, this.isDutch);
     }
     return null;
@@ -54,9 +85,34 @@ export default class StartScreen extends Stage{
    * @param mouseListener gives the mouse as an object
    */
   public override processInput(mouseListener: MouseListener): void {
-    if (mouseListener.buttonPressed(MouseListener.BUTTON_LEFT)){
-      if(this.startButton.isCollidingWithMouse(mouseListener)){
+    if (mouseListener.buttonPressed(MouseListener.BUTTON_LEFT)) {
+      //startbutton
+      if (this.startButton.isCollidingWithMouse(mouseListener)) {
         this.started = true;
+      }
+      //gender buttons that also give selected and set gender
+      if (this.genderButtons[0]?.isCollidingWithMouse(mouseListener)){
+        this.player.setGender('boy');
+        this.selectedGender= new Button(null, this.canvas.width * 0.3375, this.canvas.height
+           * 0.275, this.selectedImage, this.canvas.width * 0.1, this.canvas.height * 0.2);
+      }else if (this.genderButtons[2]?.isCollidingWithMouse(mouseListener)){
+        this.player.setGender('nonBinary');
+        this.selectedGender= new Button(null, this.canvas.width * 0.565, this.canvas.height * 0.275,
+          this.selectedImage, this.canvas.width * 0.1, this.canvas.height * 0.2);
+      }else if (this.genderButtons[1]?.isCollidingWithMouse(mouseListener)){
+        this.player.setGender('girl');
+        this.selectedGender= new Button(null, this.canvas.width * 0.45, this.canvas.height * 0.275,
+          this.selectedImage, this.canvas.width * 0.1, this.canvas.height * 0.2);
+      }
+      //flag buttons that also give selected and set language
+      if (this.languageButtons[0]?.isCollidingWithMouse(mouseListener)){
+        this.setIsDutch(true);
+        this.selectedFlag= new Button(null, this.canvas.width * 0.3375, this.canvas.height
+           * 0.525, this.selectedImage, this.canvas.width * 0.175, this.canvas.height * 0.2);
+      }else if (this.languageButtons[1]?.isCollidingWithMouse(mouseListener)){
+        this.setIsDutch(false);
+        this.selectedFlag= new Button(null, this.canvas.width * 0.4875, this.canvas.height * 0.525,
+          this.selectedImage, this.canvas.width * 0.175, this.canvas.height * 0.2);
       }
     }
   }
@@ -75,6 +131,16 @@ export default class StartScreen extends Stage{
    */
   public override render(canvas: HTMLCanvasElement): void {
     CanvasRenderer.drawImage(canvas, this.backgroundImage, 0, 0, canvas.width, canvas.height);
+    //selected
+    this.selectedGender.render(canvas);
+    this.selectedFlag.render(canvas);
+    //buttons
     this.startButton.render(canvas);
+    this.genderButtons.forEach((gender: Button) => {
+      gender.render(canvas);
+    });
+    this.languageButtons.forEach((language: Button) => {
+      language.render(canvas);
+    });
   }
 }
