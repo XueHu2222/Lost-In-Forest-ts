6af7c18969858f357018c21d8e1e26613a9f1f5c
Sas van Damme <sasdamme@gmail.com>
2025-01-22 14:04:49 +0100

    Added theory to the game

--- src/Challenges/BiologyChallenge.ts
+++ src/Challenges/BiologyChallenge.ts
@@ -8,35 +8,95 @@ import Animal from '../Animals/Animal.js';
 export default class BiologyChallenge extends Challenge {
   public constructor(difficultyLevel: string, player: Player, isDutch: boolean) {
     super(difficultyLevel, player, isDutch);
-    const categoryData: string[][] = [];
+    const categoryData: string[][][] = [];
     let categoryNames: string[] = [];
     this.animal = new Animal(this.canvas.width * 0.8, this.canvas.height * 0.66, 'monkey', 4);
 
     // Pushes the correct data for each category based on the difficulty
     switch (difficultyLevel) {
       case 'easy':
-        categoryData.push(['Koe / Cow', 'Schaap / Sheep', 'Varken / Pig', 'Paard / Horse']); // Farm Animals
-        categoryData.push(['Kat / Cat', 'Hond / Dog', 'Hamster', 'Konijn / Rabbit']); // Pets
-        categoryData.push(['Arend / Eagle', 'Mus / Sparrow', 'Meeuw / Seagull', 'Flamingo']); // Birds
-        categoryData.push(['Haai / Shark', 'Dolfijn / Dolphin', 'Walvis / Whale', 'Octopus']); // Ocean Animals
+        categoryData.push([
+          ['Koe / Cow', 'Een koe is een groot boerderijdier dat melk produceert. Koeien worden vaak op boerderijen gehouden omdat ze melk, vlees en leer leveren.'],
+          ['Schaap / Sheep', 'Een schaap is een wollig dier dat op boerderijen leeft. Ze worden gehouden voor hun wol, die wordt gebruikt om kleding te maken, en voor hun vlees.'],
+          ['Varken / Pig', 'Een varken is een roze of bruin boerderijdier met een snuit. Varkens worden gefokt voor hun vlees, zoals spek en varkensvlees.'],
+          ['Paard / Horse', 'Een paard is een groot dier dat snel kan rennen en mensen kan dragen. Paar-den worden op boerderijen gebruikt voor werk, zoals het trekken van karren.']]
+        ); // Farm Animals
+        categoryData.push([
+          ['Kat / Cat', 'Een kat is een klein, harig dier dat spint en veel slaapt. Katten zijn pop-ulaire huisdieren omdat ze vriendelijk en gemakkelijk te verzorgen zijn.'],
+          ['Hond / Dog', 'Een hond is een trouw dier dat kan blaffen. Honden worden als huisdier     gehouden omdat ze loyaal, beschermend en geweldige metgezellen zijn.'],
+          ['Hamster', 'Een hamster is een klein, harig knaagdier met kleine pootjes. Hamsters zijn populaire huisdieren omdat ze gemakkelijk te verzorgen zijn.'],
+          ['Konijn / Rabbit', 'Een konijn is een klein dier met lange oren dat huppelt. Konijnen worden   als huisdieren gehouden omdat ze zachtaardig en leuk zijn.']]
+        ); // Pets
+        categoryData.push([
+          ['Arend / Eagle', 'Een arend is een grote vogel met scherpe klauwen en een haakvormige snavel. Arenden zijn krachtige vogels die hoog kunnen vliegen en jagen.'],
+          ['Mus / Sparrow', 'Een mus is een kleine, bruine vogel die in steden en tuinen voorkomt.      Mussen zijn veel voorkomende vogels die vaak dicht bij mensen leven.'],
+          ['Meeuw / Seagull', 'Een meeuw is een witte vogel die vaak bij de oceaan wordt gezien. Meeuwen  zijn goed in het vinden van voedsel op stranden.'],
+          ['Flamingo', 'Een flamingo is een lange, roze vogel met lange poten. De flamingo is een  unieke vogel vanwege hun roze veren en hun liefde voor water.']]
+        ); // Birds
+        categoryData.push([
+          ['Haai / Shark', 'Een haai is een grote vis met scherpe tanden. Haaien leven in de oceaan en behoren tot de top-roofdieren in het water.'],
+          ['Dolfijn / Dolphin', 'Een dolfijn is een slim en speels zeedier. Dolfijnen leven in de oceaan,   zwemmen snel en staan bekend om hun intelligentie.'],
+          ['Walvis / Whale', 'Een walvis is een gigantisch zeezoogdier dat lucht ademt. Walvissen leven  in de oceaan en zijn enkele van de grootste dieren ter wereld.'],
+          ['Octopus', 'Een octopus is een zeedier met acht armen. Octopussen leven in de oceaan en zijn bijzonder omdat ze kunnen camoufleren en zich in kleine ruimtes      kunnen wurmen.']]
+        ); // Ocean Animals
 
         categoryNames = ['Farm Animals', 'Pets', 'Birds', 'Ocean Animals'];
         break;
 
       case 'medium':
-        categoryData.push(['Leeuw / Lion', 'Dolfijn / Dolphin', 'Vleermuis / Bat', 'Olifant / Elephant']); // Mammals
-        categoryData.push(['Krokodil / Crocodile', 'Hagedis / Lizard', 'Slang / Snake', 'Schildpad / Turtle']); // Reptiles
-        categoryData.push(['Lieveheersbeestje / Ladybug', 'Rups / Caterpillar', 'Mier / Ant', 'Bij / Bee']); // Insects
-        categoryData.push(['Frog', 'Toad', 'Salamander', 'Newt']); // Amphibians
+        categoryData.push([
+          ['Leeuw / Lion', 'Een leeuw is naast de tijger de allergrootste kat, vaak de "koning van de  jungle" genoemd. Leeuwen zijn zoogdieren omdat ze hun jongen melk geven.'],
+          ['Dolfijn / Dolphin', 'Een dolfijn is een speels zeedier. Dolfijnen zijn zoogdieren omdat ze lucht ademen en hun jongen met melk voeden.'],
+          ['Vleermuis / Bat', 'Een vleermuis is een klein vliegend dier dat in de nacht actief is. Vleer- muizen zijn zoogdieren omdat ze bont hebben en hun jongen melk geven.'],
+          ['Olifant / Elephant', 'Een olifant is het grootste landzoogdier. Olifanten zijn zoogdieren omdat  ze haar hebben, warmbloedig zijn en levende jongen baren.']]
+        ); // Mammals
+        categoryData.push([
+          ['Krokodil / Crocodile', 'Een krokodil is een groot reptiel met scherpe tanden en schubbenhuid.      Krokodillen zijn reptielen omdat ze schubben hebben en eieren leggen.'],
+          ['Hagedis / Lizard', 'Een hagedis is een klein reptiel dat kan kruipen en klimmen. Het zijn rep- tielen omdat ze koudbloedig zijn en een droge, schubbige huid hebben.'],
+          ['Slang / Snake', 'Een slang is een lang reptiel zonder poten dat kronkelt. Slangen zijn      reptielen omdat ze schubben hebben en eieren leggen.'],
+          ['Schildpad / Turtle', 'Een schildpad is een langzaam reptiel met een hard schild. Schildpadden    zijn reptielen omdat ze schubben hebben, eieren leggen en op land of in het water leven.']]
+        ); // Reptiles
+        categoryData.push([
+          ['Lieveheersbeestje / Ladybug', 'Een lieveheersbeestje is een klein, rood insect met zwarte stippen. Het    zijn insecten omdat ze zes poten en drie lichaamsdelen hebben.'],
+          ['Rups / Caterpillar', 'Een rups is de jonge vorm van een vlinder. Rupsen zijn insecten omdat ze   zes poten hebben en zich ontwikkelen tot vlinders.'],
+          ['Mier / Ant', 'Een mier is een klein, hardwerkend insect dat in kolonies leeft. Het zijn  insecten omdat ze zes poten, voelsprieten hebben en grote groepen vormen.'],
+          ['Bij / Bee', 'Een bij is een zoemend insect dat honing maakt. Bijen zijn insecten omdat  ze zes poten hebben en belangrijk zijn voor planten en bloemen.']]
+        ); // Insects
+        categoryData.push([
+          ['Frog', 'Een kikker is een klein dier dat in water en op het land leeft en daarom   zijn het ook amfibieën.'],
+          ['Toad', 'Een pad heeft een drogere huid en kortere poten dan een kikker. Het zijn   amfibieën omdat ze op het land leven maar hun eieren in water leggen.'],
+          ['Salamander', 'Een salamander is een klein dier een gladde huid heeft. Salamanders zijn   amfibieën omdat ze water nodig hebben om hun huid vochtig te houden.'],
+          ['Newt', 'Een watersalamander is een type kleine salamander. Watersalamanders zijn   amfibieën omdat ze hun leven in water beginnen en later op het land kunnen leven.']]
+        ); // Amphibians
 
         categoryNames = ['Mammals', 'Reptiles', 'Insects', 'Amphibians'];
         break;
 
       case 'hard':
-        categoryData.push(['Paard / Horse', 'Koe / Cow', 'Giraf / Giraffe', 'Konijn / Rabbit']); // Herbivores
-        categoryData.push(['Tijger / Tiger', 'Wolf / Wolf', 'Haai / Shark', 'Leeuw / Lion']); // Carnivores
-        categoryData.push(['Beer / Bear', 'Beer / Bear', 'Mens / Human', 'Wasbeer / Raccoon']); // Omnivores
-        categoryData.push(['Mier / Ant', 'Egel / Hedgehog', 'Vogel / Bird', 'Kameleon / Chameleon']); // Insectivores
+        categoryData.push([
+          ['Paard / Horse', 'Een paard is een groot dier dat gras en hooi eet. Paarden zijn herbivoren  omdat ze alleen planten eten zoals gras en bladeren, om energie te krijgen.'],
+          ['Koe / Cow', 'Een koe is een boerderijdier dat gras eet en melk levert. Het zijn herbivo-ren omdat ze een speciale maag hebben die hen helpt planten te verteren.'],
+          ['Giraf / Giraffe', 'Een giraf is het hoogste landdier. Giraffen zijn herbivoren omdat ze alleen planten eten, en hun lange nek gebruiken om bij hun voedsel te komen.'],
+          ['Konijn / Rabbit', 'Een konijn is een klein dier dat huppelt en groenten eet. Konijnen zijn    herbivoren omdat ze gras, groenten en planten eten om gezond te blijven.']]
+        ); // Herbivores
+        categoryData.push([
+          ['Tijger / Tiger', 'Een tijger is een grote, gestreepte kat die jaagt op andere dieren. Tijgers zijn carnivoren omdat ze vlees eten om te overleven..'],
+          ['Wolf / Wolf', 'Een wolf is een wilde hond die in groepen jaagt. Wolven zijn carnivoren    omdat ze jagen en vlees eten, zoals herten, konijnen en kleinere dieren.'],
+          ['Haai / Shark', 'Een haai is een krachtige zee-roofdier met scherpe tanden. Haaien zijn     carnivoren omdat ze andere vissen en zeedieren eten om te overleven.'],
+          ['Leeuw / Lion', 'Een leeuw is een grote kat die jaagt op dieren op de grasvlakten. Leeuwen  zijn carnivoren omdat ze jagen en dieren eten, zoals gazellen.']]
+        ); // Carnivores
+        categoryData.push([
+          ['Beer / Bear', 'Een beer is een groot dier. Beren zijn omnivoren omdat ze planten zoals    bessen eten, maar ook jagen op vis en kleine dieren.'],
+          ['Varken / Pig', 'Een varken is een boerderijdier dat veel soorten voedsel eet. Varkens zijn omnivoren omdat ze zowel planten als vlees eten. '],
+          ['Mens / Human', 'Een mens is een persoon, en wij maken deel uit van het dierenrijk. Mensen  zijn omnivoren omdat we zowel planten als vlees eten om energie te krijgen.'],
+          ['Wasbeer / Raccoon', 'Een wasbeer is een klein dier dat  in de nacht vaak naar voedsel zoekt. Was-beren zijn omnivoren omdat ze zowel planten, als kleine dieren eten.']]
+        ); // Omnivores
+        categoryData.push([
+          ['Miereneter / Anteater', 'Een miereneter is een groot dier met een lange snuit en een tong. Het zijn insectivoren omdat ze voornamelijk mieren eten om hun voedsel te krijgen.'],
+          ['Egel / Hedgehog', 'Een egel is een klein, stekelig dier dat zich oprolt voor bescherming. Het zijn insectivoren omdat ze insecten, zoals kevers en wormen eten.'],
+          ['Vogel / Bird', 'Sommige vogels, zoals roodborstjes, eten insecten als hun belangrijkste    voedsel. Vogels kunnen insectivoren zijn omdat veel van hen insecten eten.'],
+          ['Kameleon / Chameleon', 'Kameleons zijn insectivoren omdat ze insecten, zoals vliegen en krekels,   vangen met hun lange, kleverige tongen.']]
+        ); // Insectivores
 
         categoryNames = ['Herbivores', 'Carnivores', 'Omnivores', 'Insectivores'];
         break;
--- src/Challenges/HistoryChallenge.ts
+++ src/Challenges/HistoryChallenge.ts
@@ -7,28 +7,88 @@ import Animal from '../Animals/Animal.js';
 export default class HistoryChallenge extends Challenge {
   public constructor(difficultyLevel: string, player: Player, isDutch: boolean) {
     super(difficultyLevel, player, isDutch);
-    const categoryData: string[][] = [];
+    const categoryData: string[][][] = [];
     this.animal = new Animal(this.canvas.width * 0.8, this.canvas.height * 0.66, 'frog', 5);
 
     // Pushes the correct data for each category based on the difficulty
     switch (difficultyLevel) {
       case 'easy':
-        categoryData.push(['Tanks', 'Anne Frank', 'Duitsland / Germany', 'Soldaten / Soldiers']); // World War 2
-        categoryData.push(['Kastelen / Castles', 'Ridders / Knights', 'Koning / Kings', 'Koningin / Queen']); // The Dark Ages
-        categoryData.push(['Romeinen / The Romans', 'Grieken / The Greeks', 'Gladiatoren / Gladiators', 'Colosseum']); // Antiquity
-        categoryData.push(['Mammoet / Mammoth', 'Vuur / Fire', 'Grotten / Caves', 'Steen / Stone']); // Prehistory
+        categoryData.push([
+          ['Tanks', 'Een tank is een zwaar gepantserd voertuig met een kanon en rupsbanden,     essentieel in WOII voor mobiele vuurkracht en het doorbreken van linies.'],
+          ['Anne Frank', 'Anne Frank was een Joods meisje dat een dagboek schreef terwijl ze         ondergedoken zat voor de nazis, het dagboek gaf inzicht in haar leven.'],
+          ['Duitsland / Germany', 'Duitsland, onder leiding van Hitler, begon WOII en viel veel landen in     Europa binnen, wat het een sleutelrol in het conflict gaf.'],
+          ['Soldaten / Soldiers', 'Soldaten vochten in veldslagen tijdens de oorlog, verdedigden hun landen ennamen deel aan militaire operaties op verschillende fronten.']]
+        ); // World War 2
+        categoryData.push([
+          ['Kastelen / Castles', 'Kastelen waren grote stenen gebouwen waar koningen, koninginnen en ridders woonden en bescherming boden tijdens gevechten. '],
+          ['Ridders / Knights', 'Ridders waren geharnaste soldaten die dienden voor koningen en koninginnen, te paard gingen en het land en de mensen verdedigden.'],
+          ['Koning / Kings', 'Koningen regeerden over landen, in kastelen waar ze toezicht hielden op het land en de bevolking.'],
+          ['Koningin / Queen', 'Koninginnen waren heersers of de vrouwen van koningen, die hielpen bij het beheer van het land.']]
+        ); // The Dark Ages
+        categoryData.push([
+          ['Romeinen / The Romans', 'De Romeinen waren mensen uit het oude Rome, bekend om het bouwen van wegen, steden en monumenten zoals het Colosseum.'],
+          ['Grieken / The Greeks', 'De Grieken kwamen uit het oude Griekenland, beroemd om hun kunst en ideeën, en begonnen de Olympische Spelen en bouwden indrukwekkende tempels.'],
+          ['Gladiatoren / Gladiators', 'Gladiatoren waren vechters in het oude Rome die het publiek vermaakten door in grote arenas zoals het Colosseum tegen elkaar of dieren te vechten.'],
+          ['Colosseum', 'Het Colosseum, een enorme stenen arena in Rome, was belangrijk voor        gladiatorengevechten en publieke spektakels in het oude Rome.']]
+        ); // Antiquity
+        categoryData.push([
+          ['Mammoet / Mammoth', 'Een mammoet was een groot, harig dier zoals een olifant, waarop vroege     mensen jaagden voor voedsel, vacht en botten.'],
+          ['Vuur / Fire', 'Vuur geeft warmte en licht, het was essentieel voor vroege mensen om te    koken, warm te blijven en zich te beschermen tegen roofdieren.'],
+          ['Grotten / Caves', 'Grotten zijn natuurlijke ruimtes in rotsen of bergen waar vroege mensen    woonden voor veiligheid en grotschilderingen maakten.'],
+          ['Steen / Stone', 'Stenen zijn harde rotsen die vroege mensen gebruikten om gereedschappen en onderkomens te maken, essentieel voor hun overleving.']]
+        ); // Prehistory
         break;
       case 'medium':
-        categoryData.push(['D-Day', 'Holocaust', 'Atoombom / Atomic Bomb', '1939']); // World War 2
-        categoryData.push(['Kruistochten / Crusades', 'Monniken / Monks', 'Adel / Nobility', 'De Pest / The Plague']); // The Dark Ages
-        categoryData.push(['Olympische Spelen / Olympic Games', 'Mythologie / Mythology', 'Julius Caesar', 'Filosofie / Philosophy']); // Antiquity
-        categoryData.push(['Homo Sapiens', 'Jagers / Hunters', 'Boeren / Farmers', 'Verzamelaars / Gatherers']); // Prehistory
+        categoryData.push([
+          ['D-Day', 'D-Day was een belangrijke veldslag op 6 juni 1944. Op die dag gingen solda-ten uit verschillende landen naar Frankrijk om Duitsland te verslaan.'],
+          ['Holocaust', 'De Holocaust was een tragische gebeurtenis tijdens de Tweede Wereldoorlog, waarbij miljoenen Joodse mensen en anderen werden vervolgd of gedood.'],
+          ['Atoombom / Atomic Bomb', 'Een atoombom is een krachtig wapen dat hele steden kan verwoesten. In 1945 werden twee bommen op Japan gegooid, wat het einde van de oorlog betekende.'],
+          ['1939', '1939 markeerde het begin van de Tweede Wereldoorlog toen Duitsland Polen   binnenviel, wat leidde tot een wereldomvattend conflict dat duurde tot 1945']]
+        ); // World War 2
+        categoryData.push([
+          ['Kruistochten / Crusades', 'De kruistochten waren lange reizen waarbij ridders naar het Midden-Oosten  gingen om de heilige stad Jeruzalem te veroveren.'],
+          ['Monniken / Monks', 'Monniken waren mannen die in kloosters leefden. Ze bewaakten kennis tijdens de Middeleeuwen door boeken te schrijven en les te geven.'],
+          ['Adel / Nobility', 'Edelen waren rijke en machtige landeigenaren die in kastelen woonden, over boeren en ridders heersten en wetten maakten om hun land te beschermen.'],
+          ['De Pest / The Plague', 'De pest, ook wel de Zwarte Dood genoemd, was een dodelijke ziekte die zich over Europa verspreidde en miljoenen mensen doodde.']]
+        ); // The Dark Ages
+        categoryData.push([
+          ['Olympische Spelen / Olympic Games', 'De Olympische Spelen begonnen in het oude Griekenland als sportwedstrijden om Zeus, de koning van de Griekse goden, te eren.'],
+          ['Mythologie / Mythology', 'Mythologie is de verzameling van verhalen over goden, helden en wezens,    verteld door de Grieken en Romeinen om de natuur en het leven uit te leggen.'],
+          ['Julius Caesar', 'Julius Caesar was een machtige leider van het oude Rome die gebieden       veroverde, Rome versterkte en heerser werd voordat hij werd vermoord.'],
+          ['Filosofie / Philosophy', 'Filosofie is de studie van kennis en grote vragen over het leven.']]
+        ); // Antiquity
+        categoryData.push([
+          ['Homo Sapiens', 'Homo sapiens waren vroege mensen die het meest op ons van vandaag lijken.  Ze jaagden op dieren, verzamelden voedsel en maakten gereedschap.'],
+          ['Jagers / Hunters', 'Jagers waren mensen die dieren vingen voor voedsel, en gebruikten          gereedschap zoals speren en bogen om te jagen voor hun overleving.'],
+          ['Boeren / Farmers', 'Boeren verbouwden gewassen en hielden dieren voor voedsel, waardoor mensen op één plek konden blijven en dorpen konden bouwen.'],
+          ['Verzamelaars / Gatherers', 'Verzamelaars verzamelden planten, vruchten, noten en bessen om te eten, en trokken rond om voedsel te vinden voordat de landbouw begon.']]
+        ); // Prehistory
         break;
       case 'hard':
-        categoryData.push(['Blitzkrieg', 'Kristallnacht', 'Geallieerden / Allies', 'Pearl Harbor']); // World War 2
-        categoryData.push(['Karel de Grote / Charlemagne', 'Horigen / Serfs', 'Leenstelsel / Loan System', 'Vikingen / Vikings']); // The Dark Ages
-        categoryData.push(['Alexander de Grote / Alexander the Great', 'Democratie / Democracy', 'Akropolis / Acropolis', 'Cleopatra']); // Antiquity
-        categoryData.push(['Neanderthaler', 'Grotschilderingen / Cave Paintings', 'Nomaden / Nomads', 'Stenen Werktuigen / Stone Tools']); // Prehistory
+        categoryData.push([
+          ['Blitzkrieg', 'Blitzkrieg, dat "bliksemoorlog" betekent, was een strategie waarbij het    Duitse leger snel aanviel om veldslagen snel te winnen.'],
+          ['Kristallnacht', 'Kristallnacht, of de "Nacht van het Gebroken Glas", was een tragische      gebeurtenis in 1938 waarin Joodse huizen en gebouwen werden verwoest.'],
+          ['Geallieerden / Allies', 'De geallieerden, waaronder de VS, Groot-Brittannië en de Sovjet-Unie,      werkten samen om Duitsland en zijn bondgenoten te verslaan'],
+          ['Pearl Harbor', 'Pearl Harbor, in Hawaï, werd op 7 december 1941 aangevallen door Japan, wat leidde tot de deelname van de VS aan de Tweede Wereldoorlog.']]
+        ); // World War 2
+        categoryData.push([
+          ['Karel de Grote / Charlemagne', 'Karel de Grote, een koning die over een groot deel van Europa regeerde,    verenigde landen en bracht onderwijs en orde terug tijdens de Middeleeuwen.'],
+          ['Horigen / Serfs', 'Horigen waren boeren die op het land van edelen werkten, voedsel verbouwden en niet zonder toestemming mochten vertrekken.'],
+          ['Leenstelsel / Loan System', 'Het leenstelsel gaf koningen de mogelijkheid om land aan edelen te geven in ruil voor trouw en bescherming, wat zorgde voor organisatie.'],
+          ['Vikingen / Vikings', 'Vikingen, dappere zeevaarders en krijgers uit Scandinavië verkenden, handelden en vielen soms Europese steden en dorpen aan.']]
+        ); // The Dark Ages
+        categoryData.push([
+          ['Alexander de Grote / Alexander the Great', 'Alexander de Grote een koning van het oude Griekenland creëerde een van de grootste rijken in de geschiedenis en verspreidde de Griekse cultuur.'],
+          ['Democratie / Democracy', 'Democratie, uitgevonden door de oude Grieken, geeft mensen de mogelijkheid om te stemmen en beslissingen te beïnvloeden, en wordt nog steeds gebruikt.'],
+          ['Akropolis / Acropolis', 'De Akropolis, een heuvel in Athene met tempels zoals het Parthenon, was het culturele en religieuze centrum van het oude Athene.'],
+          ['Cleopatra', 'Cleopatra, een koningin van het oude Egypte, was invloedrijk en bekend om   haar relaties met Romeinse leiders zoals Julius Caesar en Marcus Antonius.']]
+        ); // Antiquity
+        categoryData.push([
+          ['Neanderthaler', 'Neanderthalers waren vroege mensen uit de ijstijd die jaagden, gereedschap maakten en in grotten leefden.'],
+          ['Grotschilderingen / Cave Paintings', 'Grotschilderingen, door vroege mensen op muren gemaakt, tonen dieren en het dagelijks leven en zijn een van de eerste kunstvormen.'],
+          ['Nomaden / Nomads', 'Nomaden trokken rond om voedsel en onderdak te vinden. Ze jaagde op dieren en verzamelde zonder lang op één plek te blijven.'],
+          ['Stenen Werktuigen / Stone Tools', 'Stenen werktuigen, gemaakt van rotsen, waren cruciaal voor vroege mensen om te jagen, vlees te snijden en onderdak te bouwen.']]
+        ); // Prehistory
         break;
     }
     const categoryNames: string[] = ['World War 2', 'The Dark Ages', 'Antiquity', 'Prehistory'];
