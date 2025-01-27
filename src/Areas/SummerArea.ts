bb024781aa0b7ce5e8de20046750044b2bf8fb05
Miguel Brugge <brug0024@hz.nl>
2025-01-27 12:48:50 +0100

    Fixed dialog removed medium

--- src/Areas/SummerArea.ts
+++ src/Areas/SummerArea.ts
@@ -44,7 +44,7 @@ export default class SummerArea extends Area {
   protected override initiateDialog(): void {
     this.animalDialogue = [
       [...this.separateDialogIntoArrays(LostInTheForest.locale.t('Hoot hoot, welkom.')), [LostInTheForest.locale.t('(....Klik om door te gaan)')]],
-      [...this.separateDialogIntoArrays(LostInTheForest.locale.t('Als je een stuk van de sleutel wilt|moet je mijn aardrijkskunde|uitdaging voltooien op medium.')), [LostInTheForest.locale.t('(....Klik om door te gaan)')]],
+      [...this.separateDialogIntoArrays(LostInTheForest.locale.t('Als je een stuk van de sleutel wilt|moet je mijn aardrijkskunde|uitdaging voltooien.')), [LostInTheForest.locale.t('(....Klik om door te gaan)')]],
       [...this.separateDialogIntoArrays(LostInTheForest.locale.t('Veel plezier!')), [LostInTheForest.locale.t('(....klik op de knop om door te gaan)')]],
     ];
   }
--- src/Areas/WinterArea.ts
+++ src/Areas/WinterArea.ts
@@ -44,7 +44,7 @@ export default class WinterArea extends Area {
   protected override initiateDialog(): void {
     this.animalDialogue = [
       [...this.separateDialogIntoArrays(LostInTheForest.locale.t('Hihi, hallo.')), [LostInTheForest.locale.t('(....Klik om door te gaan)')]],
-      [...this.separateDialogIntoArrays(LostInTheForest.locale.t('Als je een stuk van de sleutel wilt|hebben van mij dan moet je|mijn biologie uitdaging|voltooien op medium.')), [LostInTheForest.locale.t('(....Klik om door te gaan)')]],
+      [...this.separateDialogIntoArrays(LostInTheForest.locale.t('Als je een stuk van de sleutel wilt|hebben van mij dan moet je|mijn biologie uitdaging|voltooien.')), [LostInTheForest.locale.t('(....Klik om door te gaan)')]],
       [...this.separateDialogIntoArrays(LostInTheForest.locale.t('Je kan het!')), [LostInTheForest.locale.t('(....klik op de knop om door te gaan)')]],
     ];
   }
